7aca18bed9d97014cb42b4a122d43017cb49df75
klukasik <konrad.lukasik@interia.pl>
2016-04-16 19:19:07 +0200

    Saving of configuration to app.config

--- AmbientHue/HueConfiguration.cs
+++ AmbientHue/HueConfiguration.cs
@@ -12,6 +12,7 @@
             }
             set
             {
+                this.Save("IP", value);
                 ConfigurationManager.AppSettings["IP"] = value;
             }
         }
@@ -24,6 +25,7 @@
             }
             set
             {
+                this.Save("AppKey", value);
                 ConfigurationManager.AppSettings["AppKey"] = value;
             }
 
@@ -37,11 +39,21 @@
             }
             set
             {
+                this.Save("LightName", value);
                 ConfigurationManager.AppSettings["LightName"] = value;
             }
         }
 
         public bool IsCapturePossible => string.IsNullOrEmpty(this.IP) == false && string.IsNullOrEmpty(this.AppKey) == false
                                          && string.IsNullOrEmpty(this.LightName) == false;
+
+        private void Save(string key, string value)
+        {
+            var config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            config.AppSettings.Settings[key].Value = value;
+            config.Save(ConfigurationSaveMode.Modified);
+
+            ConfigurationManager.RefreshSection("appSettings");
+        }
     }
 }
